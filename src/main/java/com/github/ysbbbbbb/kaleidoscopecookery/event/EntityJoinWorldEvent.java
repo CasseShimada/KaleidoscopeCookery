29e33b094b7a52c6ad4d41b7f47fa35277e823d4
CasseShimada <c382304209@gmail.com>
2026-04-19 16:59:43 +0800

    Avoid duplicating entity goals on reload

--- src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/EntityJoinWorldEvent.java
+++ src/main/java/com/github/ysbbbbbb/kaleidoscopecookery/event/EntityJoinWorldEvent.java
@@ -11,14 +11,23 @@ import net.minecraft.world.entity.animal.feline.Cat;
 import net.minecraft.world.entity.monster.Creeper;
 
 public class EntityJoinWorldEvent {
+    private static final String CAT_LIE_GOAL_TAG = "kaleidoscope_cookery.cat_lie_goal";
+    private static final String CREEPER_MUSTARD_AVOID_TAG = "kaleidoscope_cookery.creeper_mustard_avoid_goal";
+
     public static void register() {
         ServerEntityEvents.ENTITY_LOAD.register(EntityJoinWorldEvent::onEntityJoinWorld);
     }
 
     private static void onEntityJoinWorld(Entity entity, ServerLevel level) {
         if (entity instanceof Cat cat) {
+            if (!entity.addTag(CAT_LIE_GOAL_TAG)) {
+                return;
+            }
             cat.goalSelector.addGoal(5, new CatLieOnBlockGoal(cat, 1.1, 8));
         } else if (entity instanceof Creeper creeper) {
+            if (!entity.addTag(CREEPER_MUSTARD_AVOID_TAG)) {
+                return;
+            }
             creeper.goalSelector.addGoal(3, new AvoidEntityGoal<>(creeper, LivingEntity.class, 6,
                     1, 1.2, e -> e.hasEffect(ModEffects.MUSTARD)));
         }
